3e614591dc58bbfb945b18bd50c53f42e7f4c278
BeedoProgrammer <abdelrahmanh38@gmail.com>
2024-09-03 20:14:51 +0300

    Bind IP and Port to socket and Start communication with server

--- Client.py
+++ Client.py
@@ -1,8 +1,21 @@
 import socket as sk
 import random as rn
+import time
 
-client_IP = "192.168.1.8"
-client_port = 12345
+IP = "192.168.1.8"
+port = 12345
 packet_number = 0
 
 sock = sk.socket(sk.AF_INET,sk.SOCK_DGRAM)
+sock.bind((IP, port))
+
+print("Connection with server started")
+
+while True:
+    sock.sendto(str(packet_number).encode(), (IP, port))
+    print(f"Client sent {packet_number} to {IP}:{port}")
+
+    data, address = sock.recvfrom(1024)
+    print(f"Client received {data.decode()} from {address}")
+    
+    
