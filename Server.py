4e32718d9acff9c4cbe8f209de65e70e2eef4b9c
BeedoProgrammer <abdelrahmanh38@gmail.com>
2024-09-03 18:24:20 +0300

    Import random and Open communication

--- Server.py
+++ Server.py
@@ -1,6 +1,16 @@
 import socket as sk
+import random as ran
 
-ip = "192.168.1.8"
+IP = "192.168.1.8"
 port = 12345
+packet_number = 0
 
 sock = sk.socket(sk.AF_INET,sk.SOCK_DGRAM)
+
+sock.bind((IP,port))
+sock.listen(5) 
+
+
+while True:
+    print(f"Server sending {packet_number} to {IP}:{port}")
+    
